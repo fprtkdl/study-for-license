9e07be766889e6635d2f23437e47640692cd7430
fprtkdl <beatpunchbeef141211@gmail.com>
2025-12-03 17:21:59 +0900

    fix
    대소문자와 공백이 정답에 영향을 주지 않게 수정

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let count = 0;
 let correctAnswer = 0;
 const answer = document.querySelector(".answer");
 const nextBtn = document.querySelector(".next-btn");
-// const maxNumber = Number(location.href.split("?")[1]);
-const maxNumber = 3; // 점수 확인 버튼 테스트용
+const maxNumber = Number(location.href.split("?")[1]);
+// const maxNumber = 3; // 점수 확인 버튼 테스트용
 const passageBox = document.querySelector(".passage-box");
 const resultCase = document.querySelector(".result-case");
 const testScoreBtn = document.querySelector(".test-score-btn");
@@ -43,7 +43,7 @@ function printTest() {
       const index = generateRandomNumber();
       // const index = ; 특정 문제 출력용
       jsonPassage = data[index].passage;
-      jsonAnswer = data[index].answer;
+      jsonAnswer = data[index].answer.replace(/ /g, "").toLowerCase();
 
       passageBox.append(
         createElement("number fwb", `no.${index + 1}`),
@@ -82,7 +82,7 @@ nextBtn.addEventListener("click", () => {
   switch (nextBtnClickCount) {
     case 1:
       count++;
-      checkAnswer(answer.value);
+      checkAnswer(answer.value.replace(/ /g, "").toLowerCase());
       break;
     case 2:
       printTest();
